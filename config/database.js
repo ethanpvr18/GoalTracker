e8eb3b4f7d669895f131d28db1790f0cc91c6327
Ethan Pervere <ethan@Ethans-MacBook-Air.local>
2020-10-02 23:10:09 -0400

    Heroku

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    database: 'mongodb+srv://goaltracker-user-0:goaltracker-secret-password@goaltracker-rdxtu.mongodb.net/GoalTracker?retryWrites=true&w=majority',
+    database: 'mongodb+srv://goaltracker-user-0:goaltracker-secret-password@goaltracker.rdxtu.mongodb.net/GoalTracker?retryWrites=true&w=majority',
     secret: 'goaltracker-secret-key'
 }
+
