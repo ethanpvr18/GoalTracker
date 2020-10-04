704847c718bb037944dde062fff8314f248616f6
Ethan Pervere <ethan@Ethans-MacBook-Air.local>
2020-10-03 20:03:02 -0400

    Delete Confirmation

--- public/js/main.js
+++ public/js/main.js
@@ -82,4 +82,18 @@ $(document).ready(function(){
       }
     }
   });
+  $('.openModal2').on('click', function(e){
+    // Get the modal
+    var modal = document.getElementById("Modal2");
+
+    // When the user clicks on the button, open the modal
+    modal.style.display = "block";
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+  });
 });
